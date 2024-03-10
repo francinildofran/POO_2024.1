db63c4f7917b394cec90d45c2d955b8ac74f94b3
francinildofran <52641198+francinildofran@users.noreply.github.com>
2024-03-10 02:27:55 +0000

    Últimas alterações realizadas

---
+++ IntroPOO/L01/Q04.py
@@ -0,0 +1,8 @@
+class CinemaSessao:
+
+    def __init__(self, dia, horario):
+          self.dia = dia
+          self.horario = horario
+
+    def calc_entMeia(self):
+          return self.horario * 0.5
